9839fdc94a867a91d62ed7956e3d3322bbfdce1d
Anugami <narutouzumaki@Narutos-MacBook-Air.local>
2026-03-13 17:56:35 +0530

    Fix Flutter setup for Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-echo "Installing Flutter"
-
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
-
-flutter doctor
-
-echo "Fetching Flutter packages"
+# 1. Get Flutter packages and create config files
 flutter pub get
 
-echo "Generating iOS Flutter files"
-flutter build ios --debug --no-codesign
-
-echo "Installing CocoaPods"
+# 2. Setup iOS dependencies
 cd ios
-pod install --repo-update
+rm -rf Pods
+rm -rf Podfile.lock
+pod install
 cd ..
+
+# 3. Build the app
+xcodebuild build -scheme Runner \
+-workspace /Volumes/workspace/repository/ios/Runner.xcworkspace \
+-destination generic/platform=iOS \
+-derivedDataPath /Volumes/workspace/DerivedData \
+CODE_SIGN_IDENTITY=- \
+AD_HOC_CODE_SIGNING_ALLOWED=YES
