0f7c736133f0acdd341cd0f7e9ca6100975e7fa8
Anugami <narutouzumaki@Narutos-MacBook-Air.local>
2026-03-13 16:49:05 +0530

    Fix Xcode Cloud Flutter build

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,19 +2,22 @@
 set -e
 
 echo "Installing Flutter"
-
-git clone https://github.com/flutter/flutter.git -b stable
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
-flutter doctor
+echo "Flutter version"
+flutter --version
 
 echo "Getting packages"
 flutter pub get
 
-echo "Installing pods"
+echo "Cleaning project"
+flutter clean
+
+echo "Install CocoaPods"
 cd ios
-pod install
+pod install --repo-update
 cd ..
 
-echo "Generating iOS files"
-flutter build ios --no-codesign
+echo "Generate iOS build files"
+flutter build ios --debug --no-codesign
